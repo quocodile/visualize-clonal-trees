29790fb0ca230be21e80e2c3e46140e4f2683ce8
Thea Traw <trawt@carleton.edu>
2023-07-13 17:42:10 -0500

    automatically handle both DOT and newick input w/o needing a button

--- api.py
+++ api.py
@@ -22,7 +22,7 @@ def write_dot_tree_2_file(dot_tree_str, filename):
   with open(filename, "w") as f:
     f.write(dot_tree_str)
 
-def calculation_contributions_and_node_mutation_relations(distance_measure):
+def calculation_contributions_and_node_mutation_relations(distance_measure, tree1_data, tree2_data):
   """
   Required:
     - The distance_measure selected as a string 
@@ -35,27 +35,47 @@ def calculation_contributions_and_node_mutation_relations(distance_measure):
     Writes trees to t1.txt and t2.txt
     Converts Newick trees to DOT trees
   """
-  tree1_data = flask.request.args.get('tree1')
-  tree2_data = flask.request.args.get('tree2')
-  tree1_type = flask.request.args.get('treeType1')
-  tree2_type = flask.request.args.get('treeType2')
-
-  #convert trees to DOT format if needed
-  if tree1_type == "newick":
-    tree1_data = Newick_2_dot.convert_newick_2_dot(tree1_data)
-  if tree2_type == "newick":
-    tree2_data = Newick_2_dot.convert_newick_2_dot(tree2_data)
 
   tree_1_write_location = "t1.txt"
   tree_2_write_location = "t2.txt"
   write_dot_tree_2_file(tree1_data, tree_1_write_location)
   write_dot_tree_2_file(tree2_data, tree_2_write_location)
+  
 
   node_contribution_dict_1, node_contribution_dict_2, mutation_contribution_dict_1, mutation_contribution_dict_2, node_mutations_dict_1, node_mutations_dict_2, distance = dist_meas_cont.dist_main(distance_measure, tree_1_write_location, tree_2_write_location)
   #Currently, node_mutations_dict_1 and node_mutations_dict_2 are being calculated but not passed to frontend. There might be a future use for such dictionaries though (line below would pass them along).
   #jsonObject = {"node_contribution_dict_1": node_contribution_dict_1, "node_contribution_dict_2": node_contribution_dict_2, "mutation_contribution_dict_1": mutation_contribution_dict_1, "mutation_contribution_dict_2": mutation_contribution_dict_2, "node_mutations_dict_1":node_mutations_dict_1, "node_mutations_dict_2":node_mutations_dict_2, "distance": distance}
+  
   jsonObject = {"node_contribution_dict_1": node_contribution_dict_1, "node_contribution_dict_2": node_contribution_dict_2, "mutation_contribution_dict_1": mutation_contribution_dict_1, "mutation_contribution_dict_2": mutation_contribution_dict_2, "distance": distance}
+  
   return(json.dumps(jsonObject))
+
+
+def formatHandling(distance_measure):
+
+  #wrapper to handle all possible combinations of input format as well as invalid submissions
+  
+  tree1_data = flask.request.args.get('tree1')
+  tree2_data = flask.request.args.get('tree2')
+  
+  try: #assume DOT format
+    return calculation_contributions_and_node_mutation_relations(distance_measure, tree1_data, tree2_data)
+  
+  except: #try newick format (and convert to DOT)
+    try: #assume both trees are newick format
+      return calculation_contributions_and_node_mutation_relations(distance_measure, Newick_2_dot.convert_newick_2_dot(tree1_data), Newick_2_dot.convert_newick_2_dot(tree2_data))
+    
+    except: #try one tree as DOT and one tree as newick
+      try: #assume only tree 1 is newick
+        return calculation_contributions_and_node_mutation_relations(distance_measure,  Newick_2_dot.convert_newick_2_dot(tree1_data), tree2_data)
+      
+      except: #last possible combination, otherwise problematic input
+        try: #assume only tree 2 is newick
+          return calculation_contributions_and_node_mutation_relations(distance_measure, tree1_data,  Newick_2_dot.convert_newick_2_dot(tree2_data))
+
+        except:
+          raise Exception("problem in the input! :(") from None
+  
 
 @api.route('/parent_child_distance')
 def run_parent_child_distance():
@@ -73,7 +93,9 @@ def run_parent_child_distance():
     string representing tree1 and the
     string representing tree2 
   """
-  return calculation_contributions_and_node_mutation_relations("parent_child")
+  
+  return formatHandling("parent_child")
+    
   
 @api.route('/ancestor_descendant_distance')
 def run_ancestor_descendant_distance():
@@ -91,7 +113,8 @@ def run_ancestor_descendant_distance():
     string representing tree1 and the
     string representing tree2 
   """
-  return calculation_contributions_and_node_mutation_relations("ancestor_descendant")
+  
+  return formatHandling("ancestor_descendant")
 
 @api.route('/caset_distance')
 def run_caset_distance():
@@ -109,7 +132,8 @@ def run_caset_distance():
     string representing tree1 and the
     string representing tree2 
   """
-  return calculation_contributions_and_node_mutation_relations("caset")
+  
+  return formatHandling("caset")
 
 @api.route('/disc_distance')
 def run_disc_distance():
@@ -127,4 +151,5 @@ def run_disc_distance():
     string representing tree1 and the
     string representing tree2 
   """
-  return calculation_contributions_and_node_mutation_relations("disc")
+  
+  return formatHandling("disc")
